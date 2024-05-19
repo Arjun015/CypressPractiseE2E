d7cd46a60c890259ad65adfe4108b7b8d632b7ec
Arjun015 <jadhavarjun376@gmail.com>
2024-05-19 13:29:49 -0500

    sec_commit

--- cypress.config.js
+++ cypress.config.js
@@ -2,13 +2,17 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   "defaultCommandTimeout": 15000,
+  "reporter": 'cypress-mochawesome-reporter',
   e2e: {
+    //baseurl :"https://naveenautomationlabs.com/opencart/index.php?route=account/login",
     //retries :{"runMode":1,"openMode":1},
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on)
     },
   },
   env :{
+    baseurl :"https://naveenautomationlabs.com/opencart/index.php?route=account/login",
     URL :"https://naveenautomationlabs.com/opencart/index.php?route=account/register"
   }
 });
